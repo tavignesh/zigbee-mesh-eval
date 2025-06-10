0d032881165c245e39070cd5d48920e3ad33382f
Vignesh.T.A <vigneshta004@gmail.com>
2025-06-10 13:55:01 +0530

    Add files via upload

---
+++ Objects/startup_samd20.d
@@ -0,0 +1,1 @@
+.\objects\startup_samd20.o: startup_SAMD20.s
---
+++ Objects/startup_samd20_1.d
@@ -0,0 +1,4 @@
+.\objects\startup_samd20_1.o: startup_SAMD20.c
+.\objects\startup_samd20_1.o: C:\Keil_v5\ARM\PACK\Keil\SAMD20_DFP\1.1.1\Device\SAMD20\Include\samd20.h
+.\objects\startup_samd20_1.o: C:\Keil_v5\ARM\PACK\Keil\SAMD20_DFP\1.1.1\Device\SAMD20\Include\samd20j18.h
+.\objects\startup_samd20_1.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
